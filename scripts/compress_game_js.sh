2d5c18135807b7510221b7b98d96b5042486eb51
zxq <2201210535@stu.pku.edu.cn>
2022-12-02 20:55:40 +0800

    create project stucture

---
+++ scripts/compress_game_js.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+JS_PATH=/home/yoichi/acapp/game/static/js/
+JS_PATH_DIST=${JS_PATH}dist/
+JS_PATH_SRC=${JS_PATH}src/
+
+find $JS_PATH_SRC -type f -name '*.js' | sort | xargs cat > ${JS_PATH_DIST}game.js
+
+
+
