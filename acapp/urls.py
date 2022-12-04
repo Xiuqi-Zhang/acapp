0146aa52ba9b8119ad04de7028773228390269cb
zxq <2201210535@stu.pku.edu.cn>
2022-12-04 14:00:00 +0800

    write html js views urls

--- acapp/urls.py
+++ acapp/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-   # path('game/', include("")),
+    path('', include("game.urls.index")),
     path('admin/', admin.site.urls),
 ]
--- scripts/compress_game_js.sh
+++ scripts/compress_game_js.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
+#此文件将static/js下的src中的js文件合并，并存在dist文件夹中
+
 JS_PATH=/home/yoichi/acapp/game/static/js/
 JS_PATH_DIST=${JS_PATH}dist/
 JS_PATH_SRC=${JS_PATH}src/
