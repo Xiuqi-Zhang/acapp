bee37d587caf44db851e833469dbf55a487656aa
zxq <2201210535@stu.pku.edu.cn>
2022-12-10 20:43:03 +0800

    create a app on AcWing

--- acapp/settings.py
+++ acapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-zc8bxgxku$+-4o^6+$fq250t3n8c1jxv(y)*c5rt5xclgtpaz8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["152.136.180.94"]
+ALLOWED_HOSTS = ["152.136.180.94", "app4199.acapp.acwing.com.cn"]
 
 
 # Application definition
